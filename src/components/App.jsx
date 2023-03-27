de0c142ff2a1d858befb8e4de9d8d9d71edfcd5f
Vastasika <kykysia13@gmail.com>
2023-03-27 22:38:43 +0300

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,84 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { useState } from 'react';
+// import { Component } from 'react';
+import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
+import Section from './Section/Section';
+import Statistics from './Statistics/Statistics';
+
+// export class App extends Component {
+//   state = {
+//     good: 0,
+//     neutral: 0,
+//     bad: 0,
+//   };
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+
+  // handleFeedback = option => {
+  //   // this.setState(prevState => ({ [option]: prevState[option] + 1 }));
+  // };
+
+  const handleFeedback = option => {
+   switch (option) {
+    case 'good': setGood(prevState => prevState + 1)
+      break;
+      case 'neutral': setNeutral(prevState => prevState + 1)
+      break;
+      case 'bad': setBad(prevState => prevState + 1)
+      break;
+    default:
+      break;
+   }
+  }
+
+
+  const total = good + neutral + bad;
+  //   () => {
+  //   // const { good, neutral, bad } = this.state;
+  //   return good + neutral + bad;
+  // };
+
+  const positivePercent = Math.round((good / total) * 100);
+  //   () => {
+  //   // const { good } = this.state;
+  //   // const total = this.totalFeedback();
+  //    const total = totalFeedback();
+  //   return total ? Math.round((good / total) * 100) : 0;
+  // };
+
+  // render() {
+    // const { good, neutral, bad } = this.state;
+  // const total = this.totalFeedback();
+  // const total = totalFeedback();
+  // //  const percent = this.positivePercent();
+  // const percent = positivePercent();
+  
+    return (
+      <>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+          //  options={Object.keys(this.state)}
+          //   onLeaveFeedback={this.handleFeedback}
+            options={['good','neutral','bad']}
+            // {Object.keys(this.state)} 
+            onLeaveFeedback={handleFeedback}
+          />
+        </Section>
+        <Section title="Statistics">
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            percent={positivePercent}
+          />
+        </Section>
+      </>
+    );
+  // }
+}
+
+export default App;
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types';
+import Notification from 'components/Notification/Notification';
+import styled from './Statistic.module.css';
+
+const Statistics = ({ good, neutral, bad, total, percent }) => {
+  return (
+    <>
+      {total > 0 && (
+        <ul className={styled.ul}>
+          <li className={styled.li}>
+            Good:{good}
+          </li>
+          <li className={styled.li}>Neutral:{neutral}</li>
+          <li className={styled.li}>Bad:{bad}</li>
+          <li className={styled.li}>Total:{total}</li>
+          <li className={styled.li}>Percent:{percent}%</li>
+        </ul>
+      )}
+      {total === 0 && <Notification message="No feadback given" />}
+    </>
+  );
+};
+
+Statistics.propTypes = {
+  good: PropTypes.number.isRequired,
+  neutral: PropTypes.number.isRequired,
+  bad: PropTypes.number.isRequired,
+  total: PropTypes.number.isRequired,
+  percent: PropTypes.number.isRequired,
+};
+
+export default Statistics;
